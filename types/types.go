3af76461c681059baae472d9ce43e6bd041a95c2
Cameron Badman <cbadwork@gmail.com>
2026-03-01 19:07:02 +1000

    added basic types

--- types/types.go
+++ types/types.go
@@ -1,11 +1,11 @@
-// general types
+// Packages types
 package types
 
 type VhostUserMsg struct {
 	VhostUserRequest string
 	Flags            uint32
 	Size             uint32
-	QemuPacked       []byte
+	Payload          []byte
 }
 
 type VringState struct {
@@ -13,12 +13,17 @@ type VringState struct {
 	Num   uint32
 }
 
-func (v VringState) AvailIndex() uint16     { return uint16(v.Num & 0x7FFF) }
-func (v VringState) AvailWrapCounter() bool { return v.Num&(1<<15) != 0 }
-func (v VringState) UsedIndex() uint16      { return uint16((v.Num >> 16) & 0x7FFF) }
-func (v VringState) UsedWrapCounter() bool  { return v.Num&(1<<31) != 0 }
+type VringDesc struct {
+	Index      uint32
+	AvailIndex uint32
+}
 
-type VringAddr struct {
+type VringDescIndices struct {
+	Index       uint32
+	DescIndices uint32
+}
+
+type VringAddDesc struct {
 	Index      uint32
 	Flags      uint32
 	Descriptor uint64
@@ -27,68 +32,52 @@ type VringAddr struct {
 	Log        uint64
 }
 
-type MemoryRegion struct {
-	GuestAddress uint64
-	Size         uint64
-	UserAddress  uint64
-	MmapOffset   uint64
+type MemRegionDesc struct {
+	GuestAdd   uint64
+	Size       uint64
+	UserAdd    uint64
+	MmapOffset uint64
 }
 
-type MemoryRegionXen struct {
-	MemoryRegion
-	XenMmapFlags uint32
-	Domid        uint32
-}
-
-type SingleMemoryRegion struct {
+type MemRegion struct {
 	Padding uint64
-	Region  MemoryRegion
+	Region  MemRegionDesc
 }
 
-type Memory struct {
-	NumRegions uint32
-	Padding    uint32
-	Regions    [8]MemoryRegion
+type MultiMemRegions struct {
+	Num     uint32
+	Padding uint32
+	Regions [8]MemRegionDesc
 }
 
-type Log struct {
-	LogSize   uint64
-	LogOffset uint64
-}
-
-type IOTLB struct {
-	IOVA        uint64
+type IOTLBMessage struct {
+	Iova        uint32
 	Size        uint64
 	UserAddress uint64
-	Permissions uint8
+	PermFlags   uint8
 	Type        uint8
 }
 
-type Config struct {
+type VirtioDevice struct {
 	Offset  uint32
 	Size    uint32
 	Flags   uint32
 	Payload []byte
 }
 
-type VringArea struct {
+type VringAreaDesc struct {
 	U64    uint64
 	Size   uint64
 	Offset uint64
 }
 
-type Inflight struct {
+type InflightDesc struct {
 	MmapSize   uint64
 	MmapOffset uint64
 	NumQueues  uint16
 	QueueSize  uint16
 }
 
-type UUID struct {
-	Bytes [16]byte
-}
-
-type DeviceState struct {
-	TransferDirection uint32
-	MigrationPhase    uint32
+type VHostUserShared struct {
+	UUID uint16
 }
