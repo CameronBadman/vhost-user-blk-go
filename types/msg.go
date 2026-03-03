5e54bcd8f10eb70720b28e14e9224bcda2fd77d2
Cameron Badman <cbadwork@gmail.com>
2026-03-03 23:36:24 +1000

    fixed a byte issue and add msg serialiser

--- types/msg.go
+++ types/msg.go
@@ -1,8 +1,18 @@
 package types
 
+import "encoding/binary"
+
 type VhostUserMsg struct {
 	VhostUserRequest uint32
 	Flags            uint32
 	Size             uint32
 	Payload          []byte
+}
+
+func (payload *VhostUserMsg) ToBinary(buf []byte) (int, error) {
+	binary.LittleEndian.PutUint32(buf[0:], payload.VhostUserRequest)
+	binary.LittleEndian.PutUint32(buf[4:], payload.Flags)
+	binary.LittleEndian.PutUint32(buf[8:], payload.Size)
+	copy(buf[12:], payload.Payload)
+	return 12 + len(payload.Payload), nil
 }
--- types/virtio.go
+++ types/virtio.go
@@ -1,6 +1,8 @@
 // Package types with serialisation
 package types
 
+import "encoding/binary"
+
 type VirtioDevice struct {
 	Offset  uint32
 	Size    uint32
@@ -9,6 +11,10 @@ type VirtioDevice struct {
 }
 
 func (payload *VirtioDevice) ToBinary(buf []byte) (int, error) {
-	b := castToBytes(payload)
-	return copy(buf, b), nil
+	binary.LittleEndian.PutUint32(buf[0:], payload.Offset)
+	binary.LittleEndian.PutUint32(buf[4:], payload.Size)
+	binary.LittleEndian.PutUint32(buf[8:], payload.Flags)
+	copy(buf[16:], payload.Payload)
+	// 4 + 4 + 4 + x
+	return 12 + len(payload.Payload), nil
 }
