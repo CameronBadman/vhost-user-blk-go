d90147ba228f55d4b2d13779e33972bc0332ea3d
Cameron Badman <cbadwork@gmail.com>
2026-03-03 13:43:39 +1000

    added virtio and vring binary serialisers

--- types/virtio.go
+++ types/virtio.go
@@ -1,8 +1,22 @@
+// Package types with serialisation
 package types
+
+import "encoding/binary"
 
 type VirtioDevice struct {
 	Offset  uint32
 	Size    uint32
 	Flags   uint32
 	Payload []byte
+}
+
+
+
+func (payload *VirtioDevice) ToBinary(buf []byte) (int, error) {
+	binary.LittleEndian.PutUint32(buf[0:], payload.Offset)
+	binary.LittleEndian.PutUint32(buf[4:], payload.Size)
+	binary.LittleEndian.PutUint32(buf[8:], payload.Flags)
+	copy(buf[16:], payload.Payload)
+	//4 + 4 + 4 + x
+	return 12 + len(payload.Payload), nil
 }
--- types/vring.go
+++ types/vring.go
@@ -1,8 +1,17 @@
 package types
 
+import "encoding/binary"
+
 type VringState struct {
 	Index uint32
 	Num   uint32
+}
+
+func (payload *VringState) ToBinary(buf []byte) (int, error) {
+	binary.LittleEndian.PutUint32(buf[0:], payload.Index)
+	binary.LittleEndian.PutUint32(buf[4:], payload.Num)
+	// 4 + 4
+	return 8, nil
 }
 
 type VringDesc struct {
@@ -10,9 +19,23 @@ type VringDesc struct {
 	AvailIndex uint32
 }
 
+func (payload *VringDesc) ToBinary(buf []byte) (int, error) {
+	binary.LittleEndian.PutUint32(buf[0:], payload.Index)
+	binary.LittleEndian.PutUint32(buf[4:], payload.AvailIndex)
+	// 4 + 4
+	return 8, nil
+}
+
 type VringDescIndices struct {
 	Index       uint32
 	DescIndices uint32
+}
+
+func (payload *VringDescIndices) ToBinary(buf []byte) (int, error) {
+	binary.LittleEndian.PutUint32(buf[0:], payload.Index)
+	binary.LittleEndian.PutUint32(buf[4:], payload.DescIndices)
+	// 4 + 4
+	return 8, nil
 }
 
 type VringAddDesc struct {
@@ -24,8 +47,29 @@ type VringAddDesc struct {
 	Log        uint64
 }
 
+func (payload *VringAddDesc) ToBinary(buf []byte) (int, error) {
+	binary.LittleEndian.PutUint32(buf[0:], payload.Index)
+	binary.LittleEndian.PutUint32(buf[4:], payload.Flags)
+	binary.LittleEndian.PutUint64(buf[8:], payload.Descriptor)
+	binary.LittleEndian.PutUint64(buf[16:], payload.Used)
+	binary.LittleEndian.PutUint64(buf[24:], payload.Available)
+	binary.LittleEndian.PutUint64(buf[32:], payload.Log)
+	// 4 + 4 + 8 + 8 + 8 + 8 
+	return 40, nil
+}
+
+
 type VringAreaDesc struct {
 	U64    uint64
 	Size   uint64
 	Offset uint64
+}
+
+
+func (payload *VringAreaDesc) ToBinary(buf []byte) (int, error) {
+	binary.LittleEndian.PutUint64(buf[0:], payload.U64)
+	binary.LittleEndian.PutUint64(buf[8:], payload.Size)
+	binary.LittleEndian.PutUint64(buf[16:], payload.Offset)
+	//  8 + 8 + 8
+	return 24, nil
 }
