205649103a391542b1ddd59c0835a838f01645eb
Cameron Badman <cbadwork@gmail.com>
2026-03-05 09:46:42 +1000

    added basic Recv function

--- transport/recv.go
+++ transport/recv.go
@@ -1,1 +1,36 @@
 package transport
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+
+	"vhost-go/wires"
+)
+
+func (s *Socket) Recv() (*wires.VhostUserMsg, error) {
+	_, err := io.ReadFull(s.Conn, s.Buf[:12])
+	if err != nil {
+		return nil, err
+	}
+	request := binary.LittleEndian.Uint32(s.Buf[0:4])
+	flags := binary.LittleEndian.Uint32(s.Buf[4:8])
+	size := binary.LittleEndian.Uint32(s.Buf[8:12])
+	payloadSize := size + 12
+
+	if size > 4096 {
+		return nil, fmt.Errorf("msg was too large")
+	}
+
+	// check for bad payload
+	if _, err = io.ReadFull(s.Conn, s.Buf[12:payloadSize]); err != nil {
+		return nil, err
+	}
+
+	return &wires.VhostUserMsg{
+		Request: request,
+		Flags:   flags,
+		Size:    size,
+		Payload: s.Buf[12:payloadSize],
+	}, nil
+}
--- wires/msg.go
+++ wires/msg.go
@@ -3,14 +3,14 @@ package wires
 import "encoding/binary"
 
 type VhostUserMsg struct {
-	VhostUserRequest uint32
-	Flags            uint32
-	Size             uint32
-	Payload          []byte
+	Request uint32
+	Flags   uint32
+	Size    uint32
+	Payload []byte
 }
 
 func (payload *VhostUserMsg) ToBinary(buf []byte) (int, error) {
-	binary.LittleEndian.PutUint32(buf[0:], payload.VhostUserRequest)
+	binary.LittleEndian.PutUint32(buf[0:], payload.Request)
 	binary.LittleEndian.PutUint32(buf[4:], payload.Flags)
 	binary.LittleEndian.PutUint32(buf[8:], payload.Size)
 	copy(buf[12:], payload.Payload)
