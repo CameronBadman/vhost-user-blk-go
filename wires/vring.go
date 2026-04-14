c55374c814a47d4cf737c86bf1cc2af95ad317cc
Cameron Badman <cbadwork@gmail.com>
2026-04-14 13:23:00 +1000

    added parsing for vrsing indices and add Desc

--- wires/vring.go
+++ wires/vring.go
@@ -1,5 +1,7 @@
 package wires
 
+import "encoding/binary"
+
 type VringState struct {
 	Index uint32
 	Num   uint32
@@ -38,7 +40,11 @@ func (payload *VringDescIndices) ToBinary(buf []byte) (int, error) {
 	return copy(buf, b), nil
 }
 
-func ParseVringDescIndices(buf []byte) (*VringDesc, error) {
+func ParseVringDescIndices(buf []byte) (*VringDescIndices, error) {
+	return &VringDescIndices{
+		Index:       binary.LittleEndian.Uint32(buf[0:4]),
+		DescIndices: binary.LittleEndian.Uint32(buf[5:8]),
+	}, nil
 	return nil, nil
 }
 
@@ -54,6 +60,17 @@ type VringAddDesc struct {
 func (payload *VringAddDesc) ToBinary(buf []byte) (int, error) {
 	b := castToBytes(payload)
 	return copy(buf, b), nil
+}
+
+func ParseVringAddDesc(buf []byte) (*VringAddDesc, error) {
+	return &VringAddDesc{
+		Index:      binary.LittleEndian.Uint32(buf[0:4]),
+		Flags:      binary.LittleEndian.Uint32(buf[5:8]),
+		Descriptor: binary.LittleEndian.Uint64(buf[9:17]),
+		Used:       binary.LittleEndian.Uint64(buf[17:25]),
+		Available:  binary.LittleEndian.Uint64(buf[26:34]),
+		Log:        binary.LittleEndian.Uint64(buf[34:42]),
+	}, nil
 }
 
 type VringAreaDesc struct {
